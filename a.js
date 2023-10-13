78548d399486ca55a9c6cdcf8fa3545869c7c5c2
WintyWolf <126546638+WintyWolf@users.noreply.github.com>
2023-10-13 03:23:31 +0000

    Javascript 10.13

---
+++ a.js
@@ -0,0 +1,8 @@
+function TakeInput(){
+    let Username=document.getElementById("Username").value;
+    let Passport=document.getElementById("Passport").value;
+     console.log("Name",Username,"passport",Passport);
+    
+
+
+}
