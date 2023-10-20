d9caae93958550320a9b6cde158e502c0233676e
WintyWolf <126546638+WintyWolf@users.noreply.github.com>
2023-10-20 03:21:02 +0000

    ad

---
+++ login/login.js
@@ -0,0 +1,7 @@
+function Login(){
+    let ner = document.getElementById("ner").value;
+    let pass = document.getElementById("pass").value;
+    if (pass.lenght >= 8) {
+    if (ner=="admin &&")
+
+    }
